0a8d0c08b924709207e6f0b40b09fd908b790bdb
cyh789 <drsggg@naver.com>
2025-07-02 23:43:56 +0900

    FEAT : 에러 발생하여 쿠폰 발급 실패시 처리
    FailedEvent에 UserId 데이터 백업 기능 추가(배치서비스와 연계 가능)
    error 로그 출력 추가

---
+++ consumer/src/main/java/com/coupon/consumer/repository/FailedEventRepository.java
@@ -0,0 +1,7 @@
+package com.coupon.consumer.repository;
+
+import com.coupon.consumer.domain.FailedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FailedEventRepository extends JpaRepository<FailedEvent, Long> {
+}
